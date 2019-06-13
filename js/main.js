4db4da59c4909e53083091216a52569abebd7f47
tatalex <burmina7@gmail.com>
2019-06-13 22:31:31 +0300

    creates pin

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ var mapWidth = map.offsetWidth;
 var POSITION_MAX_Y = 630;
 var POSITION_MIN_Y = 130;
 
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
 
 // adds mock data
 var getRandomInRange = function (min, max) {
@@ -49,4 +51,15 @@ var allAdsCollection = generateAllAds(ADS_AMOUNT);
 
 // removes class .map--faded
 map.classList.remove('map--faded');
+
+// creates pin
+var createPin = function (ad) {
+  var pinOfMap = pinTemplate.cloneNode(true);
+  pinOfMap.style.left = (ad.location.x - PIN_WIDTH / 2) + 'px';
+  pinOfMap.style.top = (ad.location.y - PIN_HEIGHT) + 'px';
+  pinOfMap.querySelector('img').src = ad.author.avatar;
+  pinOfMap.querySelector('img').alt = ad.offer.type;
+
+  return pinOfMap;
+};
 
